53e0ea31427673238dd4574cabcece9b9ef66f43
DevonRem <DevonReminick@gmail.com>
2024-05-14 21:20:15 -0400

    started work on back-end setting up post requests for register page

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,11 +1,31 @@
+import { useState } from "react";
+
 export default function RegisterPage() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function register(ev) {
+        ev.preventDefault();
+        await fetch('http://localhost:3000/register', {
+            method: 'POST',
+            body: JSON.stringify({username, password}),
+            headers: {'Content-Type':'application/json'},
+        })
+    }
+
     return (
         <>
         
-        <form className="register">
+        <form className="register" onSubmit={register}>
             <h1>Register</h1>
-            <input type="text" placeholder="Username"></input>
-            <input type="password" placeholder="Password"></input>
+            <input type="text" 
+            placeholder="Username" 
+            value={username} 
+            onChange={ev => setUsername(ev.target.value)}></input>
+            <input type="password" 
+            placeholder="Password"
+            value={password}
+            onChange={ev => setPassword(ev.target.value)}></input>
             <button>Register</button>
         </form>
         </>
