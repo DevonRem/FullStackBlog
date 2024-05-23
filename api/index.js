da8bb925bc7c406c47cae318c5de9fe14a788cdb
DevonRem <DevonReminick@gmail.com>
2024-05-23 14:56:52 -0400

    deployment testing

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ app.use('/uploads', express.static(__dirname + '/uploads'));
 mongoose.connect(process.env.MONGODB_URI);
 
 app.get("/", (req, res) => {
-    res.json("Hello2");
+    res.json("Database is currently running");
 });
 
 
