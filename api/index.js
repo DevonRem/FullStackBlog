b898ff59478b1e9d3dff760a910110cbd16655c7
DevonRem <DevonReminick@gmail.com>
2024-05-22 20:45:45 -0400

    deployment testing

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ const salt2 = 'asw27y8228';
 
 const corsConfig = {
     credentials:true, 
-    origin:['https://full-stack-blog-frontend.vercel.app'],
+    origin:['https://fullstackblogfrontend.onrender.com/'],
     methods: ["GET", "POST", "PUT"],
 };
 
