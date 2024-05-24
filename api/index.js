50c66740e100427818d4d3e0a5d32572f8205c40
DevonRem <DevonReminick@gmail.com>
2024-05-24 14:22:01 -0400

    changed database message

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ app.use('/uploads', express.static(__dirname + '/uploads'));
 mongoose.connect(process.env.MONGODB_URI);
 
 app.get("/", (req, res) => {
-    res.json("Database is currently running");
+    res.json("Database is now running, link to frontend: https://full-stack-blog-frontend.vercel.app/");
 });
 
 
